1c899c5b3c909499b180bffc20f8caf2ab2bb4ee
Anmol Nagpal <4303310+anmolnagpal@users.noreply.github.com>
2023-02-09 20:09:24 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "id" {
-  value       = join("", azurerm_databricks_workspace.example.*.id)
+  value       = join("", azurerm_databricks_workspace.main.*.id)
   description = "Specifies the resource id of the Workspace."
 }
 
