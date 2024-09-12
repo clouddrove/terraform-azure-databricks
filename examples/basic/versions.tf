d7e75615d2cb859ed6a8f30bb0516e437cfdb101
Deepak Verma <deepak.verma@clouddrove.com>
2024-09-12 23:39:42 +0530

    fix: update workflow version and update module for azure rm version 4.0.1

---
+++ examples/basic/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.6.6"
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.89.0"
+    }
+    databricks = {
+      source = "databricks/databricks"
+    }
+  }
+}
